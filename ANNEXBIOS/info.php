5223e3fabf347f6f751538fc616380399e87a01e
Thijs Herman <64640608+Turbootzz@users.noreply.github.com>
2022-09-19 14:26:59 +0200

    info page added

---
+++ ANNEXBIOS/info.php
@@ -0,0 +1,135 @@
+<?php
+//use composer packages
+require __DIR__ . '/vendor/autoload.php';
+
+//load jsonData
+$rawJSONData = file_get_contents('data.json'); //https://annexbios.nickvz.nl/api/
+$jsonData = json_decode($rawJSONData, true);
+
+//instance mustache
+$mustache = new Mustache_Engine(array('entity_flags' => ENT_QUOTES, 'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates')));
+//load mustache template
+$movieBlockTemplate = $mustache->loadTemplate('movieBlock');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>annexBios - info</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <script src="./assets/js/script.js" defer></script>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="header">
+      <div class="header_top">
+        <img src="./assets/img/nike_logo.png">
+          <ul>
+            <li><a href="#" class="active">FILM AGENDA</a></li>
+            <li><a href="#">ALLE VESTIGINGEN</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>
+      </div>
+      <div class="header_bottom">
+        <ul>
+          <li><p>Koop Je Tickets</p></li>
+          <li><input type="text" placeholder="Search.."></li>
+          <li><button>Bestel Ticket</button></li>
+        </ul>
+      </div>
+    </div>
+    <div class="main">
+      <div class="main_content_one">
+        <div class="container">
+
+      </div>
+      </div>  
+      <div class="main_content_two">
+        <div class="main">
+          <!-- ---------- -->
+
+          <!-- images -->
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="assets/img/dora.jpg" style="width:50%">
+  <div class="text">FILM Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="assets/img/dora.jpg" style="width:50%">
+  <div class="text">FILM Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="assets/img/dora.jpg" style="width:50%">
+  <div class="text">FILM Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
+          </div>
+          <div class="main_content_one">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer_top">
+        <div class="footer_top_one">
+          <img src="./assets/img/nike_logo.png">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quia consectetur illum ullam, fugit natus.
+          </p>
+          <button>Lees Meer</button>
+        </div>
+        <div class="footer_top_two">
+          <h4>Footertext</h4>
+          <p>
+            TEXTTEXT<br>TEXTTEXT<br>TEXTTEXT<br>TEXTTEXT
+          </p>
+        </div>
+        <div class="footer_top_three">
+          <h4>Footertext</h4>
+          <p>
+            <ion-icon name="logo-facebook"></ion-icon>
+            <ion-icon name="logo-twitter"></ion-icon>
+            <ion-icon name="logo-github"></ion-icon>
+          </p>
+        </div>
+      </div>
+      <div class="footer_bottom">
+        <div class="footer_nav">
+          <ul>
+            <li><a href="#">Voorwaarden </a></li>
+            <li><a href="#">Privacy Beleid </a></li>
+            <li><a href="#">Cookie Disclaimer</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
